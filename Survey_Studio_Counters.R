8da86ebee217e6dfa4b8f2b25ea21f22ab4ce185
Mike <56391191+MikeRebel@users.noreply.github.com>
2021-10-18 12:51:08 +0300

    получение счетчиков из экспорта в фаил

--- Survey_Studio_Counters.R
+++ Survey_Studio_Counters.R
@@ -1,2 +1,26 @@
 source("functions.R", chdir = TRUE, encoding = "UTF8")
 
+
+# JSON Counters
+
+counters <- get_counters()
+counters
+My_counter <- read_counter(counters[1])
+My_counter
+My_counter_content <- fromJSON(counters[1])
+view(My_counter_content)
+
+# API counters extraction
+
+# Установить ID проекта со счетчиками
+projectID <- "18658"
+
+Headers <- add_headers(Autorisation_header,"accept: application/json")
+
+# Headers <- add_headers(auth_token = Autorisation_header)
+Headers
+PID_counter_list <- GET(paste0(SSt_API_link,"projects/",projectID,"/counters"), config=Headers)
+PID_counter_list
+PID_counter_list_decode <- content(PID_counter_list)
+PID_counter_list_decode 
+View(PID_counter_list_decode)
--- functions.R
+++ functions.R
@@ -3,12 +3,36 @@ source("setup.R", chdir = TRUE, encoding = "UTF8")
 options(scipen = 999)
 options(max.print = 9999)
 counter <- 1
-counters_path = "../counters"
+counters_path = "counters/"
+SSt_API_link <- "https://api.survey-studio.com/"
+Autorisation_header <- paste0("SS-Token = ",Token1)
 
+
+
+#передаем в функцию путь к папке и тип файлов. Возвращает все файлы этого типа в папке.
+#Не смотрит вложенные директории
+get_file_listing = function(file_list_path,file_type) {
+     File.list <- list.files(path = file_list_path, full.names = TRUE, rec = F)
+     File.list <- File.list[!file.info(File.list)$isdir]
+     File.list <- File.list[grepl(file_type, File.list)]
+     # File.list <- File.list[!grepl("_status", File.list)]
+     # File.list <- File.list[!grepl(".error", File.list)]
+     File.list
+}
 
 
 get_counters = function() {
      
+     counters <- get_file_listing(counters_path, ".json")
+     
+     counters      
           
      
+}
+
+read_counter = function(counter_to_read){
+     
+     
+     counter <- read_json(counter_to_read)
+     counter
 }

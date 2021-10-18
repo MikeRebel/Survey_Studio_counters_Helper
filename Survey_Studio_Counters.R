2578538f6f0cbb6a93971d0f4bfdf63834d03bae
Mike <56391191+MikeRebel@users.noreply.github.com>
2021-10-18 10:55:11 +0300

    начало разработки

---
+++ Survey_Studio_Counters.R
@@ -0,0 +1,2 @@
+source("functions.R", chdir = TRUE, encoding = "UTF8")
+
---
+++ functions.R
@@ -0,0 +1,14 @@
+source("init.R", chdir = TRUE, encoding = "UTF8")
+source("setup.R", chdir = TRUE, encoding = "UTF8")
+options(scipen = 999)
+options(max.print = 9999)
+counter <- 1
+counters_path = "../counters"
+
+
+
+get_counters = function() {
+     
+          
+     
+}
